704ec02254f2afd4cfde3ebb225c66832bfb8cf6
bozhang_1 <bozhang_1@tujia.com>
2018-06-23 11:23:33 +0800

    add other Control

---
+++ Podfile
@@ -0,0 +1,5 @@
+use_frameworks!
+platform:ios, '8.0'
+target 'TJBehaviourAnalysis' do
+  pod 'ZMJGanttChart'
+end
---
+++ Pods/Target Support Files/Pods-TJBehaviourAnalysis/Pods-TJBehaviourAnalysis-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TJBehaviourAnalysis : NSObject
+@end
+@implementation PodsDummy_Pods_TJBehaviourAnalysis
+@end
